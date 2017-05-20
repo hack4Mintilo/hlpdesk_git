80aa1c5bdaa89b0dd6220fc29163d9461bd15bae
hack4Mintilo <hack4Mintilo@gmail.com>
2017-05-20 20:14:01 +0200

    Create setup_git.php

---
+++ setup_git.php
@@ -0,0 +1,32 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>How to setup a git environment in any path?</title>
+  </head>
+  <body>
+
+<h2>How to setup a git environment in any path?</h2>
+<ul>
+  <li>Login to GitHub page</li>
+  <li>Copy the repository where you would like to develop your projects (e.g., proj_mintilo_python.git)</li>
+  <li>Go to your PC where you would like to store the GitHub repo (e.g., /Users/<user>/Documents/laboratory/Python on MAC)</li>
+  <li>Go to the Terminal and change the current working directory (i.e., > cd /Documents/laboratory/Python)</li>
+  <li>Clone the repository (e.g., > git clone https://github.com/hack4Mintilo/proj_mintilo_python.git)</li>
+  <li>Once the git repository is clonned to your local machine, you're ready to develop your project</li>
+</ul>
+
+<h3>How to push new files to GitHub repo?</h3>
+<ul>
+  <li>cd proj_mintilo_python</li>
+  <li>touch test321.py</li>
+  <li>git status</li>
+  <li>git add test321.py</li>
+  <li>git commit -m "adding test321.py file"</li>
+  <li>git push</li>
+</ul>
+
+
+  </body>
+</html>
